0d35d5a7ca5e1aeb2517362c79919567713f2c6f
Zeheng Li <imzehengl@gmail.com>
2022-12-21 14:38:05 -0700

    show stops and routes

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import urllib.request
 from pathlib import Path
 
+import folium
 import gtfs_kit as gk
 import streamlit as st
+from shapely.geometry import mapping
+from streamlit_folium import st_folium
 
 filename = "CT_GTFS.zip"
 st.set_page_config(page_title="yyc-gtfs", page_icon="railway_car")
@@ -20,6 +23,57 @@ else:
         )
     st.success(f"{filename} has been downloaded!")
 
-with st.spinner(f"loading {filename}..."):
+
+@st.cache
+def load_feed(path):
     feed = gk.read_feed(path, dist_units="km")
-    st.dataframe(feed.describe(), use_container_width=True)
+    return feed
+
+
+feed = load_feed(path)
+
+if st.secrets.get("describe"):
+    st.subheader("Stats")
+    with st.spinner(f"loading {filename}..."):
+        st.dataframe(feed.describe(), use_container_width=True)
+
+
+if st.secrets.get("validate"):
+    st.subheader("Validation")
+    with st.spinner(f"validating {filename}..."):
+        st.dataframe(feed.validate(), use_container_width=True)
+
+st.subheader("Stops")
+m = feed.map_stops(feed.get_stops()["stop_id"])
+st_folium(m)
+
+st.subheader("Routes")
+geometrize_routes = feed.geometrize_routes()
+duplicated = geometrize_routes["route_short_name"].duplicated()
+geometrize_routes = geometrize_routes[~duplicated]
+selected_routes = st.multiselect(
+    "Choose routes", geometrize_routes["route_short_name"].tolist()
+)
+if selected_routes:
+    # feed.map_routes() does not seem to work with streamlit
+    routes = geometrize_routes[
+        geometrize_routes["route_short_name"].isin(selected_routes)
+    ]
+    m = folium.Map((51.0447, -114.0719))
+    colors = gk.COLORS_SET2
+    color_ind = 0
+    size = len(colors)
+    for geometry in routes["geometry"]:
+        coords = mapping(geometry)["coordinates"]
+        try:
+            folium.PolyLine(
+                [(b, a) for a, b in coords], color=colors[color_ind % size]
+            ).add_to(m)
+        except ValueError:
+            for _coords in coords:
+                folium.PolyLine(
+                    [(b, a) for a, b in _coords], color=colors[color_ind % size]
+                ).add_to(m)
+        finally:
+            color_ind += 1
+    st_folium(m)
